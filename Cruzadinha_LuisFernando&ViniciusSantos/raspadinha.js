df26853f04628b25b245720a31f511b1f86cb8ae
akzv-oliveira <vinicius.soliveira@bandtec.com.br>
2020-11-15 17:44:25 -0300

    Raspadinha Gran Finale

--- Cruzadinha_LuisFernando&ViniciusSantos/raspadinha.js
+++ Cruzadinha_LuisFernando&ViniciusSantos/raspadinha.js
@@ -3,10 +3,10 @@ function mostraTotal(input) {
     var id    =    input.id;
     if (valor == id) {
         input.value = `${input.value}`;
-        input.style = `background-color:rgb(94, 230, 94); color:black;`;
-        
+        input.style = `background-color:rgb(94, 230, 94); color:black; border:1px solid`;
+
     }else{
-        input.style = `background-color:rgb(226, 45, 45)`;
+        input.style = `background-color:rgb(226, 45, 45); border:1px solid`;
         input.value = ``;
     }
 }
