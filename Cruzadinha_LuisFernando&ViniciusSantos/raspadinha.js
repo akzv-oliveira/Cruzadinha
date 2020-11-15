4f73b142760df2b409c8d87a8c1ac7de0b1d33bd
loisfg <luis.oliveira@bandtec.com.br>
2020-11-15 16:06:44 -0300

    Cruzadinha version finale

---
+++ Cruzadinha_LuisFernando&ViniciusSantos/raspadinha.js
@@ -0,0 +1,12 @@
+function mostraTotal(input) {
+    var valor =   input.value.toUpperCase();
+    var id    =    input.id;
+    if (valor == id) {
+        input.value = `${input.value}`;
+        input.style = `background-color:rgb(94, 230, 94);color:black`;
+        
+    }else{
+        input.style = `background-color:rgb(226, 45, 45)`;
+        input.value = ``;
+    }
+}
--- raspadinha.js
+++
@@ -1,9 +0,0 @@
-function mostraTotal(input) {
-    var valor =  input.value;
-    var id    =    input.id;
-    if (valor == id) {
-        input.value = `${input.value}`;
-    }else{
-        input.value = "" ;
-    }
-}
